b87d6ef579c867a0623a762cd1b2d0fdddefdc01
RenYuan <renyuan.zou@icloud.com>
2015-07-01 13:42:38 +0200

    fix image tag for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-  
+  config.serve_static_assets = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
