6451f09e4cb0a866f7a5fd5e9a786d788010ac5a
RenYuan <renyuan.zou@icloud.com>
2015-07-01 13:31:18 +0200

    add config to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
